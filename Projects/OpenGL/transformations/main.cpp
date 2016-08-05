98da475fcafb1b0b79409c73cdbb361d2c29cb6c
orionquest <mridul.aanjaneya@gmail.com>
2016-08-05 09:05:39 -0500

    update transform with time.

--- Projects/OpenGL/transformations/main.cpp
+++ Projects/OpenGL/transformations/main.cpp
@@ -160,12 +160,8 @@ int main()
 
         // generate transformation matrix
         glm::mat4 trans;
-        trans=glm::rotate(trans,glm::radians(90.0f),glm::vec3(0.0,0.0,1.0));
-        trans=glm::scale(trans,glm::vec3(0.5,0.5,0.5));
-#if 0
         trans=glm::translate(trans,glm::vec3(0.5f,-0.5f,0.0f));
-        trans=glm::rotate(trans,(GLfloat)glfwGetTime()*50.0f,glm::vec3(0.0f,0.0f,1.0f));
-#endif
+        trans=glm::rotate(trans,glm::radians((GLfloat)glfwGetTime()*50.0f),glm::vec3(0.0f,0.0f,1.0f));
 
         GLuint transform_location=glGetUniformLocation(our_shader.program,"transform");
         glUniformMatrix4fv(transform_location,1,GL_FALSE,glm::value_ptr(trans));
